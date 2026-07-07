8eb803573660f3571418af8615cd4a9b15cdc5a6
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-07 21:52:18 +0900

    🔄 SW更新 vmranf7sq

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mraneks0';
+const CACHE = 'app-mranf7sq';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
