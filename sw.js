1b184d3f43ca81e120e2043fe4ad2d9a43b74915
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-07 20:52:44 +0900

    🔄 SW更新 vmralamlj

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mrafc02e';
+const CACHE = 'app-mralamlj';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
