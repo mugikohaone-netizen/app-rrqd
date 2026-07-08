d2c4cc37836f1390f08f3a4f2454f8f833ca4d01
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-08 22:42:07 +0900

    🔄 SW更新 vmrc4n598

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mrc47rnm';
+const CACHE = 'app-mrc4n598';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
