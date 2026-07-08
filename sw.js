9fa3ea324a6adef7f0fdd725b96dcf24741d0372
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-07-08 22:11:35 +0900

    🔄 SW更新 vmrc3jvbw

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mrc3coe0';
+const CACHE = 'app-mrc3jvbw';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
